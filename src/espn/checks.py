6715e713665cf8bbd0f5a4d3b6037f53fe44ca18
Allanmidze <nickbaramid@gmail.com>
2026-08-27 13:37:25 -0400

    Created event lists and database init

--- src/espn/checks.py
+++ src/espn/checks.py
@@ -1,3 +1,5 @@
+import json
+from datetime import datetime, timezone
 import httpx
 import asyncio
 
@@ -8,5 +10,22 @@ resource = 'scoreboard'
 
 r = httpx.get(f'https://site.api.espn.com/apis/site/v2/sports/{sport}/{league}/{resource}')
 data = r.json()
+
+# Test Print of the JSON
+# print(json.dumps(data, indent=2))
+
+day = data["day"]["date"] # Gets the Day of the Events
+
+print(day) # What Day is being processed
+
+games = len(data["events"]) # Gets the Amount of Games
+
+print(games)
+
+events = data.get("events",[])
+
+for event in events:
+    if event["date"].startswith(day):
+        print(event)
 
 
